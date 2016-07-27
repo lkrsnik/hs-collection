236b9290ecb0fa52e6cc026587695c3048cadf2e
Gregor Majcen <majcn.m@gmail.com>
2016-07-27 08:03:49 +0200

    remove babel, support multiple users

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,15 @@
-import restify from 'restify'
+'use strict'
 
-import _ from 'lodash'
-import fetch from 'node-fetch'
-import cheerio from 'cheerio'
+var restify = require('restify')
+var _ = require('lodash')
+var fetch = require('node-fetch')
+var cheerio = require('cheerio')
 
 
 function getCountGroupByName(hearthPwnCollection) {
     let $ = cheerio.load(hearthPwnCollection)
     let result = {}
-    $('.card-image-item.owns-card').each((i, x) => {
+    $('.card-image-item').each((i, x) => {
       let $x = $(x)
       let cardName = $x.data('cardName')
       let cardCount = parseInt($x.find('.inline-card-count').data('cardCount'), 10)
@@ -26,43 +27,70 @@ function throwIfEmpty(o, error) {
   return o
 }
 
+function mergeCollection(collections) {
+  let result = {}
+  for (let collection of collections) {
+    for (let x in collection) {
+      result[x] = (result[x] || []).concat(collection[x])
+    }
+  }
+
+  return result
+}
+
 function getCollectionCount(username) {
   return fetch(`http://www.hearthpwn.com/members/${username}/collection`)
     .then(x => x.text())
     .then(x => getCountGroupByName(x))
-    .then(x => throwIfEmpty(x, () => new Error(`There is no cards for username: ${username}`)))
+    .then(x => throwIfEmpty(x, () => new Error(`There are no cards for username: ${username}`)))
 }
 
 function getHsJson() {
   return fetch('https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json')
     .then(x => x.json())
+    .then(x => x.filter(xx => xx.cost !== undefined)) // if exists
 }
 
-async function getCardsByUsername(req, res, next) {
-  try {
-    let collectionCount = await getCollectionCount(req.params.username)
-    let hsJson = await getHsJson()
-
-    let result = hsJson
-      .filter(x => x.cost)
-      .map(x => ({
-        'card': _.pick(x, ['id', 'name', 'set', 'playerClass', 'type', 'rarity', 'cost']),
-        'count': collectionCount[x.name]
-      }))
-
-    res.send(200, result)
-  } catch(e) {
-    res.send(400, e)
-  }
-
-  return next()
+function createResult(collectionCount, hsJson) {
+  return hsJson
+    .map(x => ({
+      'card': _.pick(x, ['id', 'name', 'set', 'playerClass', 'type', 'rarity', 'cost']),
+      'count': collectionCount[x.name]
+    }))
+    // .filter(x => x.count > 0)
+    // .filter(x => x.card.rarity === 'EPIC' || x.card.rarity === 'LEGENDARY')
+    // .sort((a,b) => a.card.cost - b.card.cost)
 }
 
+function getCardsByUsername(usernames) {
+  let collectionByUsername = usernames.map(u => getCollectionCount(u))
+  let collectionCount = Promise.all(collectionByUsername)
+    .then(x => mergeCollection(x))
+
+  let hsJson = getHsJson()
+
+  return Promise.all([collectionsPromise, hsJson])
+    .then(x => createResult(x[0], x[1]))
+}
+
+function respCardsByUsername(req, res, next) {
+  let usernames = [].concat(req.query.u)
+  let resultPromise = getCardsByUsername(usernames)
+  resp(resultPromise, res, next)
+}
+
+function resp(promise, res, next) {
+  promise
+    .then(val => res.send(val))
+    .then(() => next())
+    .catch(e => next(e))
+}
 
 var server = restify.createServer({
   name: 'HS-Collection'
 })
-server.get('api/v1/cards/:username', getCardsByUsername)
+server.use(restify.queryParser())
+server.get('api/v1/cards', respCardsByUsername)
 
 server.listen(8080, function() {
   console.log(`${server.name} listening at ${server.url}`)
