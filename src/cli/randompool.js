2e561a43ec53a2277ebe6645e6dcaeb0488cf611
Gregor Majcen <majcn.m@gmail.com>
2016-08-15 17:07:13 +0200

    print simple html with images

--- src/cli/randompool.js
+++ src/cli/randompool.js
@@ -3,11 +3,15 @@
 const players = ['majcn', 'krsniiik', 'Pandaa12']
 const noCards = 10
 
+const fetch = require('node-fetch')
 const _ = require('lodash')
 const countProvider = require('../server/count_providers/hearthpwn.js')
 
+let hsJsonPromise = fetch('https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json')
+  .then(x => x.json())
+  .then(x => x.filter(xx => 'cost' in xx))
 
-Promise.all(players.map(u => countProvider(u))).then(collections => {
+let cardNamesPromise = Promise.all(players.map(u => countProvider(u))).then(collections => {
   let counts = {}
   for (let collection of collections) {
     for (let x in collection) {
@@ -23,5 +27,15 @@ Promise.all(players.map(u => countProvider(u))).then(collections => {
   	}
   }
 
-  console.log(_.sampleSize(result, noCards))
+  return _.sampleSize(result, noCards)
+})
+
+Promise.all([hsJsonPromise, cardNamesPromise]).then(x => {
+  let hsJson = x[0]
+  let cardNames = x[1]
+
+  let cards = cardNames.map(cardName => hsJson.find(c => c.name === cardName))
+  for (let card of cards) {
+  	console.log(`<img src="http://wow.zamimg.com/images/hearthstone/cards/enus/original/${card.id}.png"/>`)
+  }
 })
